a7cd4486108a6ae0300271126c9779e647c8ba61
ChristopherChow1 <Christopher.Chow-1@ou.edu>
2023-09-22 14:57:28 -0500

    Update parser.py
    Reads json file with two dataframes.
    One for the team names and info
    The other is for the team's games schedule.

--- Data Parser/parser.py
+++ Data Parser/parser.py
@@ -5,3 +5,23 @@ Created on Mon Sep 18 15:41:13 2023
 @author: chris
 """
 
+import json
+import numpy as np
+import pandas as pd
+
+#open the json as read only
+with open('schedule.json', 'r') as f:
+    data = json.load(f) # loads the data into python
+    df = pd.json_normalize(data, record_path=['teams'], errors = 'ignore') #converts input into dataframe.
+    #note that schedule is an array
+    
+    #sorts schedule into a dataframe
+    games_df = pd.concat([pd.json_normalize(team['schedule'], meta=['index','timestamp','dayOfWeek','location', 'opponent', 'outcome','pointsScored','pointsAllowed'], errors = 'ignore') for team in data['teams']])
+    
+    
+print(df.info()) #prints out info for the entire dataframe
+print(games_df.info()) # prints out info for the team schedule
+#replace info() with head() to see the data.
+
+
+f.close()
